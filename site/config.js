60f3ca96f8e13f1e98a9c7c6194ae7633a1bc661
yoshi108-102 <ginyoku102@gmail.com>
2026-03-21 12:13:07 +0900

    update

--- site/config.js
+++
@@ -1,8 +0,0 @@
-window.APP_CONFIG = {
-  awsRegion: "ap-northeast-1",
-  cognitoDomain: "REPLACE_WITH_COGNITO_DOMAIN",
-  clientId: "REPLACE_WITH_COGNITO_CLIENT_ID",
-  redirectUri: "https://REPLACE_WITH_CLOUDFRONT_DOMAIN/callback.html",
-  logoutUri: "https://REPLACE_WITH_CLOUDFRONT_DOMAIN/",
-  scopes: ["openid", "email", "profile"],
-};
